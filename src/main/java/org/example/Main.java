5d936a7718b85e9a2c394d6ca9d5380eff514ce2
ZhouSusan <szhou089@gmail.com>
2022-11-08 18:01:46 -0800

    added template to repo

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // 1) Create a class "Drums" similar to the Piano class. Please connect Piano(child class) to Instrument(Parent) class
+        // 2) Have its sound be "Drums go boom"
+        // 3) Create a Drums object passing in the value "Drums".
+        // 4) Create a drummer class.
+        // 5) Create an drummer object that passes in the drums and the name "Emma".
+        // 6) Create a play method in the Drummer class similar to the Pianist class
+    }
+}
---
+++ src/main/java/org/example/Piano.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Piano {
+    public Piano(String name){
+//        super(name);
+    }
+
+    void sound(){
+        System.out.println("Piano keys clang");
+    }
+}
